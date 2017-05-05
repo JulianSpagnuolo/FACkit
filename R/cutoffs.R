b11423e73205d2b345d6f8915b7f9de8e67f4ef6
JulianSpagnuolo <JulianSpagnuolo@users.noreply.github.com>
2017-05-05 15:58:06 +0200

    cutoffs (NEW FUNCTION) - accessor to retrieve cutoffs from cutfind results

---
+++ R/cutoffs.R
@@ -0,0 +1,24 @@
+cutoffs <- function(cut.data, markers, component=NULL)
+{
+  #' @title cutoffs
+  #' @author Julian Spagnuolo
+  #' @param cut.data output from cutfind function
+  #' @param markers vector of marker names
+  #' @param component named vector of the specific component from which the cutoff should be extracted. Names should be identical to markers
+
+  # Unless specific component is chosen the right-hand cut from the first component is retrieved
+  if(is.null(component))
+  {
+    component <- rep(1, length(markers))
+    names(component) <- markers
+  }
+
+  cutoffs <- vector(length=length(markers))
+  names(cutoffs) <- markers
+
+  for(i in markers)
+  {
+    cutoffs[i] <- cut.data[[i]]$components[[component[i]]]$right
+  }
+  return(cutoffs)
+}
