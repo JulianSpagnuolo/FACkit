3a9d34615835f455d15131a2b604a95946431458
JulianSpagnuolo <JulianSpagnuolo@users.noreply.github.com>
2017-05-08 14:20:41 +0200

    facsnorm - renamed logical to log-linear - added log transformation step to log-linear method - added logit transformation method

--- R/facsnorm.R
+++ R/facsnorm.R
@@ -1,11 +1,11 @@
-facsnorm <- function(x, cutoffs, asinCofac=25, method=c("arcsin","logical"))
+facsnorm <- function(x, cutoffs, asinCofac=25, method=c("arcsin","loglin","logit"))
 {
   #' @title Facsnorm
   #' @author Julian Spagnuolo, Tobias Rutishauser
   #' @param x data.frame or matrix of raw data
   #' @param cutoffs vector of cutoff values. values MUST be in the order of the markers in x that they are to be applied to
   #' @param asinCofac spreading parameter for arcsin transformation. Only needs to be set if method == arcsin. Default == 25.
-  #' @param method Type of transformation to be applied, either arcsin or logical transformation
+  #' @param method Type of transformation to be applied, either arcsin ("arcsin"), log-linear ("loglin") or logit ("logit") transformation
   if(method == "arcsin")
   {
     cat("Using arcsin Transformation\n")
@@ -16,7 +16,7 @@ facsnorm <- function(x, cutoffs, asinCofac=25, method=c("arcsin","logical"))
       asinData <- asinh(sweep(x[1:ncol(x)],2,cutoffs)/asinCofac)
       return(out <- data.frame(asinData))
   }
-  if(method == "logical")
+  if(method == "loglin")
   {
     cat("Using Logical Transformation\n")
     for(i in 1:ncol(x))
@@ -31,7 +31,28 @@ facsnorm <- function(x, cutoffs, asinCofac=25, method=c("arcsin","logical"))
       }
     }
     x <- sweep(x, 2, STATS = 1, FUN = "+")
+    return(log10(x))
+  }
+  if(method == "logit")
+  {
+    cat("Using logit transformation\n")
+    # Linearise the data with 0 as minimum
+    for(i in 1:ncol(x))
+    {
+      if(sign(min(x[,i])) == -1)
+      {
+        x[,i] <- x[,i]-min(x[,i])
+      }
+      if(sign(min(x[,i])) == 1)
+      {
+        x[,i] <- x[,i]+min(x[,i])
+      }
+      # fit the data between 0 and 1
+      x[,i] <- x[,i]/max(x[,i])
 
+      # Apply logit transformation
+      x[,i] <- log10((x[,i]/(1-x[,i])))
+    }
     return(x)
   }
 }
