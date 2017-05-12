c717c972ffb299bb9ec62368081f97a6614f917d
JulianSpagnuolo <JulianSpagnuolo@users.noreply.github.com>
2017-05-12 12:35:49 +0200

    somtree (NEW FUNCTION) -builds growing SOM and constructs a tree from it

---
+++ R/somtree.R
@@ -0,0 +1,35 @@
+som.tree <- function(data, markers, shape=c("rect","hex"), maxit=500, cores=1)
+{
+  #' @author Julian Spagnuolo
+  #' @title Build Growing Self-organising Map and Construct a Tree
+  #' @param data data.frame or matrix containing expression data
+  #' @param markers vector of markers/columns in data to build the SOM with
+  #' @param shape vector determining the shape of SOM to build, either rectangular, "rect", or hexagonal, "hex".
+  #' @param maxit number of iterations the SOM building function will use, default = 500.
+  #' @param cores number of cores to using in distance computation. Default = 1, set higher if you have more than 1 cpu to use.
+  #'
+  #'
+  #' @importFrom amap Dist
+
+  cat("Training SOM\n")
+  som <- train.gsom(data=data[,markers], iterations = maxit, nhood = shape, keepdata = FALSE)
+  cat("Mapping data to SOM\n")
+  mapped <- map.gsom(gsom_object = som, df=data[,markers], retaindata = FALSE)
+
+  cat("Building tree\n")
+  adj.mat <- Dist(mapped$nodes$codes, method="euclidean", nbproc=cores)
+  full.graph <- graph.adjacency(adj.mat, mode="undirected")
+  if(graph.type == "hrg")
+  {
+    graph <- consensus_tree(graph=full.graph)
+    graph <- layout_with_fr(graph=graph, niter = maxit)
+  }
+  if(graph.type == "mst")
+  {
+    graph <- minimum.spanning.tree(full.graph)
+    graph <- layout_with_fr(graph=graph, niter=maxit)
+  }
+  return(graph)
+
+
+}
