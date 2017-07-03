57f6ccebdd60457dc6ea078cebfc474674d8cb17
JulianSpagnuolo <JulianSpagnuolo@users.noreply.github.com>
2017-07-03 13:02:13 +0200

    mystars - new function - plotting for growSOM MST results
    nodeSize
    -fixed some bugs

--- R/nodeSize.R
+++ R/nodeSize.R
@@ -17,16 +17,16 @@ nodeSize <- function(somtree, reset=FALSE, maxNodeSize=15)
 {
   if(reset == TRUE)
   {
-    somtree.res$mst$size <- rep(maxNodeSize, nrow(somtree.res$map$mapped))
+    somtree.res$map$nodes$size <- rep(maxNodeSize, nrow(somtree.res$map$mapped))
   }
   else
   {
     t <- table(somtree.res$map$mapped$bmn)
-    t <- (t/sum(t)) * nrow(somtree.res$map$mapped)
+    t <- sqrt(t)
     shift <- min(t)
     scale <- max(t - shift)
     rescaled <- maxNodeSize * (t - shift)/scale
-    somtree.res$map$nodes$size <- numeric(nrow(somtree.res$map$mapped))
+    somtree.res$map$nodes$size <- numeric(nrow(somtree.res$map$nodes$codes))
     somtree.res$map$nodes$size[as.numeric(names(t))] <- rescaled
   }
   somtree.res
