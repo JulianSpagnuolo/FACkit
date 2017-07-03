7c4b2b8954ce994991aeaa7df0987dff40dab265
JulianSpagnuolo <JulianSpagnuolo@users.noreply.github.com>
2017-07-03 11:53:34 +0200

    nodeSize - New Function - sets node sizes for drawing the SOM MST - Based heavily on the FlowSOM function

---
+++ R/nodeSize.R
@@ -0,0 +1,36 @@
+nodeSize <- function(somtree, reset=FALSE, maxNodeSize=15)
+  #'
+  #' @title Scaled Node Sizes for growing SOM
+  #' @author Julian Spagnuolo
+  #' @description
+  #' @param somtree results from calling som.tree
+  #' @param reset Logical. If TRUE, node size is reset to maxNodeSize, default = FALSE
+  #' @param maxNodeSize Maximum size for any node. Default = 15
+  #'
+  #'
+  #'
+  #'
+  #'
+  #'
+  #'
+  #'
+{
+  if(reset == TRUE)
+  {
+    somtree.res$mst$size <- rep(maxNodeSize, nrow(somtree.res$map$mapped))
+  }
+  else
+  {
+    t <- table(somtree.res$map$mapped$bmn)
+    t <- (t/sum(t)) * nrow(somtree.res$map$mapped)
+    shift <- min(t)
+    scale <- max(t - shift)
+    rescaled <- maxNodeSize * (t - shift)/scale
+    somtree.res$map$mapped$size <- numeric(nrow(somtree.res$map$mapped))
+    somtree.res$map$mapped$size[as.numeric(names(t))] <- rescaled
+  }
+  somtree.res
+}
+
+
+
