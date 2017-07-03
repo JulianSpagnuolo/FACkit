1dad78afa17b10ed3c9651496d38fa7a356a2d13
JulianSpagnuolo <JulianSpagnuolo@users.noreply.github.com>
2017-07-03 10:48:44 +0200

    som.tree - Added alpha and beta params for train.som stage - Added documentation for alpha and beta

--- R/somtree.R
+++ R/somtree.R
@@ -1,4 +1,4 @@
-som.tree <- function(data, markers, shape=c("rect","hex"), maxit=500, cores=1)
+som.tree <- function(data, markers, shape=c("rect","hex"), maxit=500, cores=1, alpha=0.9, beta=0.5)
 {
   #' @author Julian Spagnuolo
   #' @title Build Growing Self-organising Map and Construct a Tree
@@ -7,6 +7,8 @@ som.tree <- function(data, markers, shape=c("rect","hex"), maxit=500, cores=1)
   #' @param shape vector determining the shape of SOM to build, either rectangular, "rect", or hexagonal, "hex".
   #' @param maxit number of iterations the SOM building function will use, default = 500.
   #' @param cores number of cores to using in distance computation. Default = 1, set higher if you have more than 1 cpu to use.
+  #' @param alpha discount factor for the learning rate during the growing phase of the training. Values should be between 0 and 1. Default = 0.9
+  #' @param beta propagation rate. Determines the rate at which the error of a node, that cannot grow any nodes, is passed on to its neighbours. Suggested values may range between 0 and 1. Default = 0.5
   #'
   #'
   #' @importFrom amap Dist
@@ -14,7 +16,7 @@ som.tree <- function(data, markers, shape=c("rect","hex"), maxit=500, cores=1)
   results <- vector(mode="list")
   cat("Training SOM\n")
   t1 <- Sys.time()
-  som <- train.gsom(data=data[,markers], iterations = maxit, nhood = shape, keepdata = FALSE)
+  som <- train.gsom(data=data[,markers], iterations = maxit, nhood = shape, keepdata = FALSE, alpha = alpha, beta=beta)
   results$som <- som
   cat("Mapping data to SOM\n")
   results$map <- map.gsom(gsom_object = som, df=data[,markers], retaindata = FALSE)
