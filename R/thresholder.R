5837b0a4fd0a968f73fbc59b68b5334b5dbc138e
JulianSpagnuolo <JulianSpagnuolo@users.noreply.github.com>
2017-09-11 13:49:24 +0200

    thresholder -fixed some stupid mistakes

--- R/thresholder.R
+++ R/thresholder.R
@@ -1,4 +1,4 @@
-thresholder < function(data, markers, thresholds)
+thresholder <- function(data, markers, thresholds)
   #' @author Julian Spagnuolo
   #' @title Threshold Phenotyping
   #' @param data numeric matrix/data.frame. Contains FACS marker expression data
@@ -11,11 +11,11 @@ thresholder < function(data, markers, thresholds)
   {
     for(n in markers)
     {
-      if(data[i.n] >= threshold[n])
+      if(data[i,n] >= threshold[n])
       {
         results[i,n] <- 1
       }
-      if(data[i.n] < threshold[n])
+      if(data[i,n] < threshold[n])
       {
         results[i,n] <- 0
       }
